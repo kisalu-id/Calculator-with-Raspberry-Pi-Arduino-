afedebfaff895683563f47391fed9c7606ef37ea
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2025-05-29 23:52:42 +0200

    Update RPiCalc.py

--- RPiCalc.py
+++ RPiCalc.py
@@ -16,59 +16,11 @@ def main():
 
 #user input
 def read_user_input(expression, i):
-    option = "here is user input"
-
-    match option:
-        case 1:
-            expression += "1"
-        case 2:
-            expression += "2"
-        case 3:
-            expression += "3"
-        case 4:
-            expression += "4"
-        case 5:
-            expression += "5"
-        case 6:
-            expression += "6"
-        case 7:
-            expression += "7"
-        case 8:
-            expression += "8"
-        case 9:
-            expression += "9"
-        case 10:
-            expression += "0"
-        case 11:
-            print("Addition")
-            expression += "+"
-        case 12:
-            print("Substraction")
-            expression += "-"
-        case 13:
-            print("Multiplication")
-            expression += "*"
-        case 14:
-            print("Division")
-            expression += "/"
-        case 15:
-            print("Case 15 executed")
-        case 16:
-            print("Input ended, next input")
-            expression += " "
-            i += 1
-            if i > 1:
-                return expression
-            else:
-
-                read_user_input(expression, i)
-        case _:
-            print("Invalid option")
+    
 
 #calculate
 def calculate(user_input):
-    result = 5
-    return result
+    return eval(user_input) 
 
 
 #output
